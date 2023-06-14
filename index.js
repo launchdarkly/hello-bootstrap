a8c696bd3722d99a1fb9cce582ee32c56cedfa8a
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-06-14 13:59:14 -0700

    Update for node sdk version 8.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-const LaunchDarkly = require('launchdarkly-node-server-sdk');
+const LaunchDarkly = require('@launchdarkly/node-server-sdk');
 
 var ldClient = LaunchDarkly.init(process.env.LD_SDK_KEY);
 
